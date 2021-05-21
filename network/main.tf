eedc7c5a8f8fa2a47f5f24d2e2413f1ef1ce47d5
Milan Melnykov <39458844+milanmelnykov@users.noreply.github.com>
2021-05-21 17:03:53 +0300

    Update main.tf

--- network/main.tf
+++ network/main.tf
@@ -5,7 +5,6 @@ variable "availability_zone" {}
 variable "vpc_cidr_block" {}
 variable "pub_subnet_cidr_block" {}
 
-
 resource "aws_vpc" "vpc" {
   cidr_block           = var.vpc_cidr_block
   enable_dns_support   = true
@@ -59,22 +58,10 @@ resource "aws_route" "public_route_for_internet_gateway" {
   route_table_id         = aws_route_table.public_route_table.id
   gateway_id             = aws_internet_gateway.igw.id
   destination_cidr_block = "0.0.0.0/0"
-
-  /* tags = {
-    Name        = format("%s_public_route_for_internet_gateway", var.app)
-    Owner       = var.owner
-    Environment = var.environment
-  } */
 }
 
 # attaching route table
 resource "aws_route_table_association" "association_table_to_public_subnet" {
   route_table_id = aws_route_table.public_route_table.id
   subnet_id      = aws_subnet.public.id
-
-  /* tags = {
-    Name        = format("%s_association_table_to_public_subnet", var.app)
-    Owner       = var.owner
-    Environment = var.environment
-  } */
 }
